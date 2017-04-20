ab900cb9fe8f49f0df20275576281a93a92a7b10
Masud Rahman <mrahman15@bloomberg.net>
2017-04-20 16:29:00 -0400

    Implement windows example

--- app/app.m.cpp
+++ app/app.m.cpp
@@ -1,12 +1,24 @@
+#ifndef _WIN32
 #include <dlfcn.h>
-#include <iostream>
+#else
+#include <windows.h>
+#endif
 
 int main()
 {
+#ifndef _WIN32
     void *one = dlopen("1/lib1.so", RTLD_LAZY | RTLD_GLOBAL);
     void *two = dlopen("2/lib2.so", RTLD_LAZY | RTLD_GLOBAL);
     reinterpret_cast<void (*)()>(dlsym(one, "ObjectOne_print"))();
     reinterpret_cast<void (*)()>(dlsym(two, "ObjectTwo_print"))();
     dlclose(one);
     dlclose(two);
+#else
+    HMODULE one = ::LoadLibrary("1/lib1.dll");
+    HMODULE two = ::LoadLibrary("2/lib2.dll");
+    reinterpret_cast<void (*)()>(::GetProcAddress(one, "ObjectOne_print"))();
+    reinterpret_cast<void (*)()>(::GetProcAddress(two, "ObjectTwo_print"))();
+    ::FreeLibrary(one);
+    ::FreeLibrary(two);
+#endif
 }
